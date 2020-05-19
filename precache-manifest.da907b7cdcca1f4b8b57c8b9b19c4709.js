56ac6e032ff127b365fe154859133d24195bd0cb
python8u <python8u@gmail.com>
2020-05-19 16:58:21 -0400

    Updates

---
+++ precache-manifest.da907b7cdcca1f4b8b57c8b9b19c4709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c996f03da7e7c9ba84c212d736465e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4608b873c12d37aba9d3",
+    "url": "/react-portfolio/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "6e907a2dd01a4f80907d",
+    "url": "/react-portfolio/static/js/2.1b54de8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.1b54de8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4608b873c12d37aba9d3",
+    "url": "/react-portfolio/static/js/main.4899d99e.chunk.js"
+  },
+  {
+    "revision": "b53d6b38d961a048a9c4",
+    "url": "/react-portfolio/static/js/runtime-main.f17f3746.js"
+  }
+]);
